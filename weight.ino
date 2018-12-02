42d802f300de4ccdbdaca7605a0537e9f1aa787e
dracen <a.hayashi0113@gmail.com>
2018-12-02 21:10:11 +0900

    Arduino nanoで体重測定できるようにした I2C動作確認済

--- weight.ino
+++ weight.ino
@@ -1,40 +1,103 @@
-#include <Ticker.h>
 #include <Wire.h>
+#include <MsTimer2.h>
 
-#define LED 12
-#define SW 13
-#define WEIGHT 14
+#define PIN 3
+#define LED 2
+#define  SW 4
+
+#define TIMEOUT_US 30000
+
+typedef struct {
+  uint32_t stable;
+  float weight;
+} weight_t;
+
+static weight_t weights;
+
+void weights_set(uint32_t stable, float weight)
+{
+  weights.stable = stable;
+  weights.weight = weight;
+}
+
+void weight_init()
+{
+  MsTimer2::stop();
+  weights_set(0xff, 0);
+  LED_on(false);
+}
+
+int read_bits(uint32_t *bh, uint32_t *bl) {
+  int count = 0;
+  uint64_t ret = 0;
+
+  while (true) {
+    unsigned long pulse = pulseIn(PIN, HIGH, TIMEOUT_US);
+    if (pulse == 0) break;
+
+    uint32_t bit = pulse < 750 ? 1 : 0;
+    if (count < 32) {
+      bitWrite(*bh, 31 - count, bit);
+    } else if (count < 64) {
+      bitWrite(*bl, 63 - count, bit);
+    }
+    ++count;
+  }
+  return count;
+}
 
 void setup() {
-  pinMode(LED, OUTPUT);
+  pinMode(PIN, INPUT);
   pinMode(SW, INPUT);
-  pinMode(WEIGHT, INPUT);
+  pinMode(LED, OUTPUT);
   Serial.begin(9600);
+
   weight_init();
   i2c_init();
 }
 
 void loop() {
-  //  LED_on(SW_on());
+  uint32_t bh = 0;
+  uint32_t bl = 0;
+  int count = read_bits(&bh, &bl);
+  if (count == 39) {
+    eee(bh, bl);
+  }
 }
 
-/************************************************/
-#define TIMEOUT_MS 30
+static void eee(uint32_t bh, uint32_t bl) {
+  uint32_t stable = (bh >> 18) & 0x03;
+  LED_toggle();
 
-typedef struct {
-  byte stable;
-  float weight;
-} weight_t;
+  char s[64];
+  itob(s, 2, stable);
+  Serial.write(s);
+  Serial.print(" ");
 
-static weight_t weight_st;
+  itob(s, 32, bh);
+  Serial.write(s);
+  Serial.print(" ");
 
-static Ticker Ticker1;
-static Ticker Ticker2;
+  itob(s, 32, bl);
+  Serial.write(s);
+  Serial.println(" ");
 
-static uint32_t bh = 0;
-static uint32_t bl = 0;
-static uint32_t cnt = 0;
-static int start_time = 0;
+  if (stable == 0x00) {
+    weights_set(stable, 0);
+  } else if (stable == 0x01) {
+    weights_set(stable, 0);
+  } else if (stable == 0x03) {
+    float weight = (bh & 0xffff) / (float) 10;
+    Serial.print(weight);
+    Serial.println(" kg");
+    LED_on(true);
+    weights_set(stable, weight);
+    MsTimer2::set(5000, weight_init);
+    MsTimer2::start();
+  } else {
+  }
+}
+
 
 static void itob(char *s, int n, unsigned long long v) {
   if (n > 0) {
@@ -53,97 +116,7 @@ static void itob(char *s, int n, unsigned long long v) {
   *s = '\0';
 }
 
-static void eee(uint32_t bh, uint32_t bl) {
-  uint8_t stable = (bh >> 18) & 0x03;
-  if (stable == 0x03) {
-    detachInterrupt(WEIGHT);
-    Ticker2.once_ms(5000, restart);
-    LED_on(true);
-  } else {
-    LED_toggle();
-  }
-
-  char s[64];
-  itob(s, 2, (unsigned long long int) stable);
-  Serial.write(s);
-  Serial.print(" ");
-
-  itob(s, 32, bh);
-  Serial.write(s);
-  Serial.print(" ");
-
-  itob(s, 32, bl);
-  Serial.write(s);
-  Serial.println(" ");
-
-  if (stable == 0x00) {
-    weight_st.stable = 0;
-    weight_st.weight = 0;
-  } else if (stable == 0x01) {
-    weight_st.stable = 1;
-    weight_st.weight = 0;
-  } else if (stable == 0x03) {
-    float weight = (bh & 0xffff) / (float) 10;
-    weight_st.stable = 2;
-    weight_st.weight = weight;
-    Serial.print(weight);
-    Serial.println(" kg");
-  } else {
-  }
-}
-
-static void restart()
-{
-  weight_st.stable = 0xff;
-  weight_st.weight = 0;
-  attachInterrupt(WEIGHT, rise, RISING);
-  LED_on(false);
-}
-
-static void timeout() {
-  detachInterrupt(WEIGHT);
-  uint32_t bh_disp = bh;
-  uint32_t bl_disp = bl;
-  uint32_t cnt_disp = cnt;
-  bh = 0;
-  bl = 0;
-  cnt = 0;
-  attachInterrupt(WEIGHT, rise, RISING);
-
-  if (cnt_disp == 39) {
-    eee(bh_disp, bl_disp);
-  }
-}
-
-
-static void rise() {
-  detachInterrupt(WEIGHT);
-  start_time = micros();
-  attachInterrupt(WEIGHT, fall, FALLING);
-}
-
-static void fall() {
-  detachInterrupt(WEIGHT);
-  Ticker1.once_ms(TIMEOUT_MS, timeout);
-  uint32_t pulse = micros() - start_time;
-  uint8_t bit = pulse < 750 ? 1 : 0;
-  if (cnt < 32) {
-    bh |= bit << (31 - cnt);
-  } else if (cnt < 64) {
-    bl |= bit << (63 - cnt);
-  }
-  cnt++;
-  attachInterrupt(WEIGHT, rise, RISING);
-}
-
-void weight_init() {
-  weight_st.stable = 0xff;
-  weight_st.weight = 0;
-  attachInterrupt(WEIGHT, rise, RISING);
-  LED_on(false);
-}
-
-/************************************************/
+/************************************************************/
 static bool SW_on()
 {
   if (  digitalRead(SW) == HIGH) {
@@ -162,15 +135,14 @@ static void LED_on(bool on)
   }
 }
 
-
 static void LED_toggle()
 {
   static bool state = true;
   state = !state;
   LED_on(state);
 }
+/************************************************************/
 
-/************************************************/
 
 void i2c_init()
 {
@@ -196,13 +168,8 @@ void receiveEvent(int n) {
 // マスターからのリクエストに対するデータ送信
 void requestEvent() {
   Serial.println("requestEvent");
-  byte *p = (byte*)(&weight_st);
-  for (int i = 0; i < sizeof(weight_st); i++) {
+  byte *p = (byte*)(&weights);
+  for (int i = 0; i < sizeof(weights); i++) {
     Wire.write(p[i]);
   }
 }
-
-
-
-
-
